cc20957198c5d024747bf52f9dcac22ee5df8318
User <User@HOME>
2017-04-06 09:53:03 +0400

    Добавьте файлы проекта.

---
+++ Ввод/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ввод
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("Введие число:");
+
+            i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Вы ввели:" + i);
+
+            Console.ReadKey();
+        }
+    }
+}
